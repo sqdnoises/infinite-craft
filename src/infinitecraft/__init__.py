5f5889b49e4c630d22ee748b150ca27f1f81421e
sqdnoises <85015626+sqdnoises@users.noreply.github.com>
2024-02-29 02:15:55 +0530

    update code

--- src/infinitecraft/__init__.py
+++ src/infinitecraft/__init__.py
@@ -6,8 +6,15 @@ License: GNU Lesser General Public License v3.0
 
 __title__ = "infinite-craft"
 __full_title__ = "Infinite Craft"
+__description__ = "An API Wrapper of Neal's Infinite Craft game in Python for people to implement in their programs."
+__cli_description__ = "Infinite Craft Utilities"
 __author__ = "SqdNoises"
 __license__ = "GNU Lesser General Public License v3.0 (GNU LGPL-3.0)"
-__copyright__ = "Copyright (C) 2024-present SqdNoises"
+__copyright__ = "Copyright (C) 2024-present SqdNoises, Neal Agarwal"
+__homepage__ = "https://github.com/sqdnoises/infinite-craft"
 __version__ = "0.0.0"
 __display_version__ = __title__ + " " + __version__
+
+from .element import *
+from .infinitecraft import *
+from .logger import *
