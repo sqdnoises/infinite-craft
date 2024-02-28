026fc8029ac7e3386ea22257104788326dfbd4ff
sqdnoises <sqdnoises@gmail.com>
2024-02-29 00:51:11 +0530

    add initial files

---
+++ src/infinitecraft/__init__.py
@@ -0,0 +1,13 @@
+"""
+An API Wrapper of Neal's Infinite Craft game in Python for people to implement in their programs.
+Copyright (C) 2024-present SqdNoises
+License: GNU Lesser General Public License v3.0
+"""
+
+__title__ = "infinite-craft"
+__full_title__ = "Infinite Craft"
+__author__ = "SqdNoises"
+__license__ = "GNU Lesser General Public License v3.0 (GNU LGPL-3.0)"
+__copyright__ = "Copyright (C) 2024-present SqdNoises"
+__version__ = "0.0.0"
+__display_version__ = __title__ + " " + __version__
